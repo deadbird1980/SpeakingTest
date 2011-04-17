8506ab8c5273b8ef9ac37484a7c318d074b63514
joey <joeyw@reallyenglish.com>
2011-04-17 10:40:59 +0100

    add LessonPage and resources for test

---
+++ com/core/lesson/LessonPage.java
@@ -0,0 +1,13 @@
+package com.core.lesson;
+
+import java.awt.*;
+import java.net.*;
+import javax.swing.*;
+import javax.swing.border.*;
+
+
+public class LessonPage extends JPanel {
+
+    private Integer pageNO;
+
+}
